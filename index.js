32b5abf6bd8366ce9155d66c4b91543157ca800b
szvitek <gy.szvitek@gmail.com>
2024-02-22 14:18:31 +0000

    feat(Metadata): metadata app

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+require('dotenv').config();
 
-var app = express();
+const upload = multer();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +13,24 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    const { originalname: name, mimetype: type, size } = req.file;
 
-
+    return res.json({
+      name,
+      type,
+      size,
+    });
+  } catch (error) {
+    console.error;
+    res.json({
+      error: error.message,
+    });
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
